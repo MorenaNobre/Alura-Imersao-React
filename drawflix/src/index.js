800258802af9900279ce368ba69c2064e26058aa
MorenaNobre <morenagnobre@gmail.com>
2020-07-28 15:26:37 -0300

    Home/Video

--- drawflix/src/index.js
+++ drawflix/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import {  Switch,  Route,  BrowserRouter } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video'
+
+//desafio
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />
+      {/* <Route component={() => (<div>Página 404</div>)} /> */}
+    </Switch>
+  </BrowserRouter>,
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ drawflix/src/pages/cadastro/Video/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CadastroVideo() {
+    return (
+      <div>
+        Página de Cadastro de Vídeo
+      </div>
+    )
+  }
+
+  export default CadastroVideo
