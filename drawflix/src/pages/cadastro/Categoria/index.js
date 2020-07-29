6a893e9276be83699558828f06b013cf4c840cb7
MorenaNobre <morenagnobre@gmail.com>
2020-07-29 10:50:50 -0300

    day3Alura

--- drawflix/src/pages/cadastro/Categoria/index.js
+++ drawflix/src/pages/cadastro/Categoria/index.js
@@ -1,26 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../components/PageDefault'
 import { Link } from 'react-router-dom'
 
 function CadastroCategoria() {
+    const [categorias, setCategorias] = useState(['teste'])
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Illustrator')    
+
     return (
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-        <form>
+        <form onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault()
+          setCategorias([
+            ...categorias,
+            nomeDaCategoria
+          ])
+        }}>
 
+          <div>
+            <label>
+              Nome da Categoria
+              <input 
+                type="text"
+                value={nomeDaCategoria}
+                onChange={function capturaMudanca(infosDoEvento) {
+                  setNomeDaCategoria(infosDoEvento.target.value)
+                }}
+              />
+            </label>            
+          </div>
+
+          <div>
           <label>
-            Nome da Categoria
-            <input 
-              type="text"
-            />
-          </label>          
+              Descrição:
+              <textarea 
+                type="text"
+                value={nomeDaCategoria}
+                onChange={function capturaMudanca(infosDoEvento) {
+                  setNomeDaCategoria(infosDoEvento.target.value)
+                }}
+              />
+            </label>            
+          </div>    
+
+          <div>
+          <label>
+              Cor:
+              <input 
+                type="color"
+                value={nomeDaCategoria}
+                onChange={function capturaMudanca(infosDoEvento) {
+                  setNomeDaCategoria(infosDoEvento.target.value)
+                }}
+              />
+            </label>
+          </div>                
 
           <button>
               Cadastrar
             </button>
             
         </form>
+
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return (
+              <li key={`${categoria}${indice}`}>
+                {categoria}
+              </li>
+            )
+          })}
+        </ul>
 
         <Link to="/">
           Ir para Home
